083e26cbbd0897228ff1c94c6dda15f4b7e8eca5
Sundui <Sundui0423@gmail.com>
2021-09-17 08:18:55 +0800

    Reveal feature buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import "../styles/style.css";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/revealOnScroll";
 
 let mobileMenu = new MobileMenu();
+ new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
+ new RevealOnScroll(document.querySelectorAll(".testimonials"),50);
---
+++ app/assets/scripts/modules/revealOnScroll.js
@@ -0,0 +1,56 @@
+ import throttle from "lodash/throttle";
+ import  debounce   from "lodash/debounce";
+class RevealOnScroll {
+    constructor(els,revealPoint)
+    {
+        this.revealPoint= revealPoint;
+        this.itemsToReveal = els;
+        this.hideInitially();
+        this.browserHeight = window.innerHeight;
+        this.scrollThrottle = throttle(this.calcCaller ,200 ).bind(this);
+        this.events();
+    }
+    events(){
+         window.addEventListener("scroll" ,this.scrollThrottle);
+         window.addEventListener(
+             "resize", debounce(()=> {
+                 console.log("Browser iig resize hiij duuslaa");
+                 this.browserHeight= window.innerHeight;
+             },333 ) )
+    }
+    calcCaller( ){
+        
+        this.itemsToReveal.forEach(el=>{
+           if(el.isRevealed ==false)
+           console.log("Scroll функц ажиллалаа")
+           {
+         
+            this.calculateIfScrolledTo(el);
+           }
+        });
+    }
+    calculateIfScrolledTo(el){
+       
+        if(window.scrollY+this.browserHeight> el.offsetTop){
+            console.log("Tootsoolol hiigdej bna");
+            let scrollPercent = 
+        (el.getBoundingClientRect().y/this.browserHeight)*100
+        if(scrollPercent<this.revealPoint){
+            el.classList.add("reveal-item--is-visible");
+            el.isRevealed = true;
+            if(el.isLastItem){
+                window.removeEventListener("scroll", this.scrollThrottle);
+            }
+        }
+        }
+      
+    }
+    hideInitially(){
+        this.itemsToReveal.forEach(el =>{
+            el.classList.add("reveal-item") 
+            el.isRevealed = false;
+        } );
+       this.itemsToReveal[this.itemsToReveal.length-1].isLastItem=true;
+    }
+} 
+export default RevealOnScroll;
