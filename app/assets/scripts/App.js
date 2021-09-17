b5870203cee148777d0d586633e1dceb6973f337
Sundui <Sundui0423@gmail.com>
2021-09-17 09:56:47 +0800

    sticky header hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../styles/style.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/revealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 let mobileMenu = new MobileMenu();
+let stickyHeader= new StickyHeader();
  new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
  new RevealOnScroll(document.querySelectorAll(".testimonials"),50);
